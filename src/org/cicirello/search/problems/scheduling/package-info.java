44603d19c9d0b40b395409378c9cad26d94ad698
Vincent Cicirello <cicirello@users.noreply.github.com>
2020-07-13 17:09:24 -0400

    added single machine scheduling interface

---
+++ src/org/cicirello/search/problems/scheduling/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * Chips-n-Salsa: A library of parallel self-adaptive local search algorithms.
+ * Copyright (C) 2002-2020  Vincent A. Cicirello
+ *
+ * This file is part of Chips-n-Salsa (https://chips-n-salsa.cicirello.org/).
+ * 
+ * Chips-n-Salsa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Chips-n-Salsa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+ 
+/**
+ * Package of classes and interfaces related to representing
+ * and solving scheduling problems, which includes implementations
+ * of constructive heuristics for scheduling problems. 
+ * @since 1.0
+ *
+ * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
+ * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
+ * @version 7.13.2020
+ */
+package org.cicirello.search.problems.scheduling;

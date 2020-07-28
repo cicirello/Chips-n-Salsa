6f8f27c73432550ddb9902e6045224da64fca5eb
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2020-07-28 14:32:53 -0400

    Setup adjusted critical ratio heuristic

--- tests/org/cicirello/search/problems/scheduling/HeuristicTests.java
+++ tests/org/cicirello/search/problems/scheduling/HeuristicTests.java
@@ -216,6 +216,55 @@ public class HeuristicTests {
 	}
 	
 	@Test
+	public void testWeightedCriticalRatioSetupAdjusted() {
+		double e = WeightedCriticalRatioSetupAdjusted.MIN_H;
+		int highP = (int)Math.ceil(1 / e)*2;
+		int[] w =    { 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 1};
+		int[] p =    { 1, 1, 2, 4, 8, 1, 2, 4, 8, 1, 2, 4, 8, highP};
+		double[] expected0 = { 999, 1, 0.5, 0.25, 0.125, e, e, e, e, 2, 1, 0.5, 0.25, e};
+		double[] slack = new double[p.length];
+		for (int i = 1; i < p.length; i++) slack[i] = 20-p[i]-p[0];
+		PartialPermutation partial = new PartialPermutation(expected0.length);
+		//Doesn't really matter: partial.extend(0);
+		// All late tests
+		FakeProblemWeightsPTime problem = new FakeProblemWeightsPTime(w, p, 0);
+		WeightedCriticalRatioSetupAdjusted h = new WeightedCriticalRatioSetupAdjusted(problem);
+		IncrementalEvaluation inc = h.createIncrementalEvaluation();
+		inc.extend(partial, 0);
+		for (int j = 1; j < expected0.length; j++) {
+			assertEquals("negativeSlack, j:"+j, expected0[j], h.h(partial, j, inc), 1E-10);
+		}
+		// All on time tests
+		problem = new FakeProblemWeightsPTime(w, p, 20);
+		h = new WeightedCriticalRatioSetupAdjusted(problem);
+		inc = h.createIncrementalEvaluation();
+		inc.extend(partial, 0);
+		for (int j = 1; j < expected0.length; j++) {
+			double expected = expected0[j]/(1.0+slack[j]/p[j]);
+			if (slack[j] <= 0) expected = e;
+			assertEquals("positiveSlack, j:"+j, expected < e ? e : expected, h.h(partial, j, inc), 1E-10);
+		}
+		// Repeat with setups
+		int[] ps =    { 0, 0, 1, 3, 7, 0, 1, 3, 7, 0, 1, 3, 7, highP-1};
+		problem = new FakeProblemWeightsPTime(w, ps, 0, 1);
+		h = new WeightedCriticalRatioSetupAdjusted(problem);
+		inc = h.createIncrementalEvaluation();
+		inc.extend(partial, 0);
+		for (int j = 1; j < expected0.length; j++) {
+			assertEquals("negativeSlack, j:"+j, expected0[j], h.h(partial, j, inc), 1E-10);
+		}
+		problem = new FakeProblemWeightsPTime(w, ps, 20, 1);
+		h = new WeightedCriticalRatioSetupAdjusted(problem);
+		inc = h.createIncrementalEvaluation();
+		inc.extend(partial, 0);
+		for (int j = 1; j < expected0.length; j++) {
+			double expected = expected0[j]/(1.0+slack[j]/p[j]);
+			if (slack[j] <= 0) expected = e;
+			assertEquals("positiveSlack, j:"+j, expected < e ? e : expected, h.h(partial, j, inc), 1E-10);
+		}
+	}
+	
+	@Test
 	public void testMontagne() {
 		double e = Montagne.MIN_H;
 		int highP = (int)Math.ceil(1 / e)*2;

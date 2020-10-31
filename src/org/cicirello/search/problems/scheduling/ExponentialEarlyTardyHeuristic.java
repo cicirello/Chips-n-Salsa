9786dcd7ff08282c17ecd461caed81a24f96efa1
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2020-10-31 18:49:57 -0400

    Added missing documentation

--- src/org/cicirello/search/problems/scheduling/ExponentialEarlyTardyHeuristic.java
+++ src/org/cicirello/search/problems/scheduling/ExponentialEarlyTardyHeuristic.java
@@ -25,6 +25,45 @@ import org.cicirello.search.ss.Partial;
 import org.cicirello.search.ss.IncrementalEvaluation;
 
 /**
+ * <p>This class implements a constructive heuristic, known as EXPET, for scheduling problems
+ * involving minimizing the sum of weighted earliness plus weighted tardiness.  EXPET
+ * is an acronym for Exponential Early Tardy.</p>
+ *
+ * <p>To define the EXPET heuristic, first let he[j] be the 
+ * weighted longest processing time heuristic
+ * of job j, defined as he[j] = -we[j] / p[j], where we[j] is the earliness weight
+ * for job j, and p[j] is the processing time of job j.  
+ * Next, let ht[j] be the weighted shortest
+ * processing time heuristic of job j, defined as ht[j] = wt[j] / p[j], where wt[j] is the
+ * tardiness weight of job j.  Define the slack s[j] of job j as:
+ * s[j] = d[j] - T - p[j], where d[j] is the job's due date and T is the
+ * current time.  Let k &ge; 1 be a lookahead parameter that can be tuned based on 
+ * problem instance characteristics, and p&#772; is the average processing
+ * time of remaining unscheduled jobs.</p>
+ *
+ * <p>Now we can define the EXPET heuristic, h[j] for job j, as follows.  
+ * Case 1: If s[j] &le; 0, 
+ * h[j] = ht[j]. Case 2: if s[j] &ge; k*p&#772;, h[j] = he[j].  
+ * Case 3: If 0 &lt; s[j] &le; k*p&#772;*ht[j]/(ht[j]-he[j]), then
+ * h[j] = ht[j] * exp((s[j](ht[j]-he[j]))/(he[j]*k*p&#772;)).
+ * Case 4: If k*p&#772;*ht[j]/(ht[j]-he[j]) &lt; s[j] &lt; k*p&#772;, then
+ * h[j] = he[j]<sup>-2</sup> * (ht[j] - s[j](ht[j]-he[j])/(k*p&#772;))<sup>3</sup>.
+ * For jobs with negative slack, the
+ * EXPET heuristic is equivalent to weighted shortest processing time.  For jobs with slack
+ * greater than some multiple k of the average processing time, EXPET is equivalent to 
+ * weighted longest processing time.</p>
+ *
+ * <p>We make one additional adjustment to the heuristic as it was originally described.
+ * Since this library's implementations of stochastic 
+ * sampling algorithms assumes that constructive
+ * heuristics always produce positive values, we must 
+ * adjust the values produced by the EXPET heuristic.
+ * Specifically, we actually compute h'[j] = h[j] + shift, where
+ * shift = {@link #MIN_H} - min(we[j] / p[j]).
+ * The {@link #MIN_H}
+ * is a small non-zero value.  In this way, we shift all of the h[j] values
+ * by a constant amount such that all h[j] values are positive.</p>
+ *
  *
  * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
  * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>

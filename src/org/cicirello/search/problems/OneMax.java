f82a78e72756f22aee09ef2b18d48d883742863e
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-03-18 13:26:42 -0400

    documentation update

--- src/org/cicirello/search/problems/OneMax.java
+++ src/org/cicirello/search/problems/OneMax.java
@@ -1,6 +1,6 @@
 /*
  * Chips-n-Salsa: A library of parallel self-adaptive local search algorithms.
- * Copyright (C) 2002-2020  Vincent A. Cicirello
+ * Copyright (C) 2002-2021  Vincent A. Cicirello
  *
  * This file is part of Chips-n-Salsa (https://chips-n-salsa.cicirello.org/).
  * 
@@ -33,18 +33,40 @@ import org.cicirello.search.representations.BitVector;
  * where the standard form of a genetic algorithm represents solutions
  * to the problem with a string of bits.  The OneMax problem offers
  * a test problem with a known optimal solution, a bit-string of all
- * 1s.  For example, if n=8, then the optimal solution is: 11111111.</p>
+ * 1s.  For example, if n=8, then the optimal solution is: 11111111.
+ * The OneMax problem has no local optima, and thus should be trivially
+ * easy for hill climbers.</p>
  *
- * <p>The {@link #value value} method simply counts the number
- * of bits in the BitVector equal to 1.  The problem is to maximize this count.  Thus, as 
+ * <p>It was originally posed as a maximization problem because 
+ * it was originally defined as a fitness function for a genetic algorithm.
+ * The {@link #value value} method simply counts the number
+ * of bits in the BitVector equal to 1, which is to be maximized. Thus, as 
  * a cost function, the {@link #cost cost} method counts the number
- * of bits not equal to 1, where the minimum cost is thus 0.</p>
+ * of bits not equal to 1, where the minimum cost is thus 0, corresponding
+ * to the case of maximal number of 1-bits.</p>
  *
- * @since 1.0
+ * <p>The OneMax problem was introduced by Ackley (1987). His original
+ * definition of the problem was to maximize: f(x) = 10 * CountOfOneBits(x).
+ * Thus, Ackley's original OneMax multiplied the number of 1-bits by 10.
+ * Our implementation leaves out the 10. Doing so does not change the
+ * optimal solution or the shape of the landscape. However, it may have
+ * an effect on the behavior of some search algorithms. For example, 
+ * simulated annealing decides whether or not to accept a worsening move
+ * with a probability that depends on the difference in cost between the current
+ * solution and the random neighbor, as well as on its current temperature.
+ * Keeping all else the same and scaling the cost values can lead to 
+ * different acceptance probabilities (for a specific temperature value).
+ * If you want to use Ackley's original version, or any other scaling for that
+ * matter, you can use 
+ * the {@link org.cicirello.search.problems.IntegerCostFunctionScaler 
+ * IntegerCostFunctionScaler} class for this purpose. You can do so by 
+ * defining your optimization problem with something like:
+ * IntegerCostFunctionScaler&lt;BitVector&gt; problem = 
+ * new IntegerCostFunctionScaler&lt;BitVector&gt;(new OneMax());</p>
  *
  * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
  * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
- * @version 6.11.2020
+ * @version 3.18.2021
  */
 public final class OneMax implements IntegerCostOptimizationProblem<BitVector> {
 	

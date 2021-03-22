c60f8101174fae4eae3679efc93c53c2a2774ada
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-03-22 14:21:48 -0400

    Update AdjacentSwapMutation.java

--- src/org/cicirello/search/operators/permutations/AdjacentSwapMutation.java
+++ src/org/cicirello/search/operators/permutations/AdjacentSwapMutation.java
@@ -1,6 +1,6 @@
 /*
  * Chips-n-Salsa: A library of parallel self-adaptive local search algorithms.
- * Copyright (C) 2002-2020  Vincent A. Cicirello
+ * Copyright (C) 2002-2021  Vincent A. Cicirello
  *
  * This file is part of Chips-n-Salsa (https://chips-n-salsa.cicirello.org/).
  * 
@@ -33,11 +33,9 @@ import org.cicirello.math.rand.RandomIndexer;
  * length of the permutation is n, there are n-1 such adjacent pairs.  The runtime of both
  * the {@link #mutate(Permutation) mutate} and {@link #undo(Permutation) undo} methods is O(1).
  *
- * @since 1.0
- *
  * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
  * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
- * @version 10.4.2019
+ * @version 3.22.2021
  */
 public final class AdjacentSwapMutation implements UndoableMutationOperator<Permutation>, IterableMutationOperator<Permutation> {
 

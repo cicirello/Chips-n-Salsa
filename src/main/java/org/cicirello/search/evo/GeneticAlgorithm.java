7205889e8d876b2f4366addfffa29efd8bfa4dc7
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-12-21 11:04:34 -0500

    refactored: extracted specialized classes
    #333

--- src/main/java/org/cicirello/search/evo/GeneticAlgorithm.java
+++ src/main/java/org/cicirello/search/evo/GeneticAlgorithm.java
@@ -26,10 +26,12 @@ import org.cicirello.search.operators.Initializer;
 import org.cicirello.search.operators.CrossoverOperator;
 import org.cicirello.search.operators.bits.BitVectorInitializer;
 import org.cicirello.search.operators.bits.BitFlipMutation;
-import org.cicirello.search.operators.bits.SinglePointCrossover;
 
 
-public final class GeneticAlgorithm extends GenerationalEvolutionaryAlgorithm<BitVector> {
+
+
+
+public class GeneticAlgorithm extends GenerationalEvolutionaryAlgorithm<BitVector> {
 	
 	// Constructors with all possible parameters, including Initializer.
 	
@@ -108,7 +110,7 @@ public final class GeneticAlgorithm extends GenerationalEvolutionaryAlgorithm<Bi
 	 * @throws NullPointerException if any of crossover, f, selection, or tracker are null.
 	 */
 	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Double<BitVector> f, double mutationRate, CrossoverOperator<BitVector> crossover, double crossoverRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, crossover, crossoverRate, new BitVectorInitializer(bitLength), f, selection, tracker);
+		this(n, new BitVectorInitializer(bitLength), f, mutationRate, crossover, crossoverRate, selection, tracker);
 	}
 	
 	/**
@@ -134,161 +136,14 @@ public final class GeneticAlgorithm extends GenerationalEvolutionaryAlgorithm<Bi
 	 * @throws NullPointerException if any of crossover, f, selection, or tracker are null.
 	 */
 	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Integer<BitVector> f, double mutationRate, CrossoverOperator<BitVector> crossover, double crossoverRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, crossover, crossoverRate, new BitVectorInitializer(bitLength), f, selection, tracker);
+		this(n, new BitVectorInitializer(bitLength), f, mutationRate, crossover, crossoverRate, selection, tracker);
 	}
-	
-	// The Simple GA: bit flip mutation, single-point crossover, and fitness proportionate selection.
-	
-	/**
-	 * <p>Initializes a simple genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation, single-point crossover (the {@link SinglePointCrossover} class), 
-	 * and fitness-proportional selection (the {@link FitnessProportionalSelection} class).
-	 * This constructor supports fitness functions
-	 * with fitnesses of type double, the {@link FitnessFunction.Double} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param bitLength The length of each bit vector.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param crossoverRate The probability that a pair of parents undergo crossover.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws IllegalArgumentException if crossoverRate is less than 0.
-	 * @throws IllegalArgumentException if bitLength is negative.
-	 * @throws NullPointerException if any of f, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Double<BitVector> f, double mutationRate, double crossoverRate, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, new SinglePointCrossover(), crossoverRate, new BitVectorInitializer(bitLength), f, new FitnessProportionalSelection(), tracker);
-	}
-	
-	/**
-	 * <p>Initializes a simple genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation, single-point crossover (the {@link SinglePointCrossover} class), 
-	 * and fitness-proportional selection (the {@link FitnessProportionalSelection} class).
-	 * This constructor supports fitness functions
-	 * with fitnesses of type int, the {@link FitnessFunction.Integer} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param bitLength The length of each bit vector.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param crossoverRate The probability that a pair of parents undergo crossover.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws IllegalArgumentException if crossoverRate is less than 0.
-	 * @throws IllegalArgumentException if bitLength is negative.
-	 * @throws NullPointerException if any of f, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Integer<BitVector> f, double mutationRate, double crossoverRate, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, new SinglePointCrossover(), crossoverRate, new BitVectorInitializer(bitLength), f, new FitnessProportionalSelection(), tracker);
-	}
-	
-	// Mutation-only GA, with Initializer.
-	
-	/**
-	 * <p>Initializes a mutation-only genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation. All other characteristics, such as 
-	 * selection operator are configurable. This constructor supports fitness functions
-	 * with fitnesses of type double, the {@link FitnessFunction.Double} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param initializer An initializer for generating random initial population members.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param selection The selection operator.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws NullPointerException if any of initializer, f, selection, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, Initializer<BitVector> initializer, FitnessFunction.Double<BitVector> f, double mutationRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, initializer, f, selection, tracker);
-	}
-	
-	/**
-	 * <p>Initializes a mutation-only genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation. All other characteristics, such as 
-	 * selection operator are configurable. This constructor supports fitness functions
-	 * with fitnesses of type int, the {@link FitnessFunction.Integer} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param initializer An initializer for generating random initial population members.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param selection The selection operator.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws NullPointerException if any of initializer, f, selection, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, Initializer<BitVector> initializer, FitnessFunction.Integer<BitVector> f, double mutationRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, initializer, f, selection, tracker);
-	}
-	
-	// Mutation-only GA, with bitLength specified.
-	
-	/**
-	 * <p>Initializes a mutation-only genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation. All other characteristics, such as 
-	 * selection operator are configurable. This constructor supports fitness functions
-	 * with fitnesses of type double, the {@link FitnessFunction.Double} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param bitLength The length of each bit vector.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param selection The selection operator.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws IllegalArgumentException if bitLength is negative.
-	 * @throws NullPointerException if any of f, selection, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Double<BitVector> f, double mutationRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, new BitVectorInitializer(bitLength), f, selection, tracker);
-	}
-	
-	/**
-	 * <p>Initializes a mutation-only genetic algorithm with a generational model where children replace the parents,
-	 * using the standard bit flip mutation. All other characteristics, such as 
-	 * selection operator are configurable. This constructor supports fitness functions
-	 * with fitnesses of type int, the {@link FitnessFunction.Integer} interface.</p>
-	 *
-	 * @param n The population size.
-	 * @param bitLength The length of each bit vector.
-	 * @param f The fitness function.
-	 * @param mutationRate The per-bit probability of flipping a bit. Each bit of each member of the 
-	 *    population is flipped with this probability, and the decisions to flip bits are independent.
-	 * @param selection The selection operator.
-	 * @param tracker A ProgressTracker.
-	 *
-	 * @throws IllegalArgumentException if n is less than 1.
-	 * @throws IllegalArgumentException if mutationRate &le; 0 or if mutationRate &ge; 1.
-	 * @throws IllegalArgumentException if bitLength is negative.
-	 * @throws NullPointerException if any of f, selection, or tracker are null.
-	 */
-	public GeneticAlgorithm(int n, int bitLength, FitnessFunction.Integer<BitVector> f, double mutationRate, SelectionOperator selection, ProgressTracker<BitVector> tracker) {
-		super(n, new BitFlipMutation(mutationRate), mutationRate > 0.0 ? 1.0 : 0.0, new BitVectorInitializer(bitLength), f, selection, tracker);
-	}
-	
-	
 	
 	/*
 	 * Internal constructor for use by split method
+	 * package private so subclasses in same package can use it for initialization for their own split methods.
 	 */
-	private GeneticAlgorithm(GeneticAlgorithm other) {
+	GeneticAlgorithm(GeneticAlgorithm other) {
 		super(other);
 		// Just call super constructor to perform split() logic. This
 		// subclass doesn't currently maintain any additional state.

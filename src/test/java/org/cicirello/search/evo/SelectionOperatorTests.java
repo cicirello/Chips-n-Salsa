de2d9b601c4ba3f880e186a25e469007e10ddf8a
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-02-10 18:17:30 -0500

    Linear Rank Selection with SUS

--- src/test/java/org/cicirello/search/evo/SelectionOperatorTests.java
+++ src/test/java/org/cicirello/search/evo/SelectionOperatorTests.java
@@ -92,6 +92,82 @@ public class SelectionOperatorTests {
 	}
 	
 	@Test
+	public void testLinearRankSUS() {
+		double[] cValues = {1.0, 1.25, 1.5, 1.75, 2.0};
+		for (double c : cValues) {
+			LinearRankStochasticUniversalSampling selection = new LinearRankStochasticUniversalSampling(c);
+			validateIndexes_Double(selection);
+			validateIndexes_Integer(selection);
+			LinearRankStochasticUniversalSampling selection2 = selection.split();
+			validateIndexes_Double(selection2);
+			validateIndexes_Integer(selection2);
+		}
+		LinearRankStochasticUniversalSampling selection = new LinearRankStochasticUniversalSampling(2.0);
+		validateHigherFitnessSelectedMoreOften_Double(selection);
+		validateHigherFitnessSelectedMoreOften_Integer(selection);
+		
+		LinearRankStochasticUniversalSampling selectionUniform = new LinearRankStochasticUniversalSampling(1.0);
+		
+		for (int n = 2; n <= 8; n *= 2) {
+			PopFitVectorDouble f1 = new PopFitVectorDouble(n);
+			double[] weights = selection.computeWeightRunningSum(f1);
+			assertEquals(0.0, weights[0], 1E-10);
+			assertEquals(n, weights[n-1], 1E-10);
+			double expectedDelta = 2.0;
+			assertEquals(expectedDelta, weights[n-1] - weights[n-2], 1E-10);
+			for (int i = n-2; i > 0; i--) {
+				double delta = weights[i] - weights[i-1];
+				assertTrue(delta <= expectedDelta);
+				expectedDelta = delta;
+			}
+			
+			weights = selectionUniform.computeWeightRunningSum(f1);
+			for (int i = 0; i < weights.length; i++) {
+				assertEquals(i+1.0, weights[i], 1E-10);
+			}
+			
+			validateExpectedCountsSUSWithRanks(selection, f1);
+			validateExpectedCountsSUSWithRanks(selectionUniform, f1);
+			
+			PopFitVectorInteger f2 = new PopFitVectorInteger(n);
+			weights = selection.computeWeightRunningSum(f2);
+			assertEquals(0.0, weights[0], 1E-10);
+			assertEquals(n, weights[n-1], 1E-10);
+			expectedDelta = 2.0;
+			assertEquals(expectedDelta, weights[n-1] - weights[n-2], 1E-10);
+			for (int i = n-2; i > 0; i--) {
+				double delta = weights[i] - weights[i-1];
+				assertTrue(delta <= expectedDelta);
+				expectedDelta = delta;
+			}
+			
+			weights = selectionUniform.computeWeightRunningSum(f2);
+			for (int i = 0; i < weights.length; i++) {
+				assertEquals(i+1.0, weights[i], 1E-10);
+			}
+			
+			validateExpectedCountsSUSWithRanks(selection, f2);
+			validateExpectedCountsSUSWithRanks(selectionUniform, f2);
+		}
+		
+		PopFitVectorDoubleSimple f1 = new PopFitVectorDoubleSimple(100);
+		validateExpectedCountsSUSWithRanks(selection, f1);
+		validateExpectedCountsSUSWithRanks(selectionUniform, f1);
+		PopFitVectorIntegerSimple f2 = new PopFitVectorIntegerSimple(100);
+		validateExpectedCountsSUSWithRanks(selection, f2);
+		validateExpectedCountsSUSWithRanks(selectionUniform, f2);
+		
+		IllegalArgumentException thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> new LinearRankStochasticUniversalSampling(2.0+Math.ulp(2.0))
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> new LinearRankStochasticUniversalSampling(1.0-Math.ulp(1.0))
+		);
+	}
+	
+	@Test
 	public void testRandomSelection() {
 		RandomSelection selection = new RandomSelection();
 		validateIndexes_Double(selection);
@@ -828,6 +904,50 @@ public class SelectionOperatorTests {
 		for (int i = 0; i < pf.size(); i++) {
 			expectedMin[i] = (int)(pf.size() * bias.bias(pf.getFitness(i)) / totalFitness);
 			expectedMax[i] = (int)Math.ceil(pf.size() * bias.bias(pf.getFitness(i)) / totalFitness);
+			assertTrue("i:"+i+" count:"+counts[i]+" min:"+expectedMin[i], counts[i] >= expectedMin[i]);
+			assertTrue("i:"+i+" count:"+counts[i]+" max:"+expectedMax[i], counts[i] <= expectedMax[i]);
+		}
+	}
+	
+	private void validateExpectedCountsSUSWithRanks(StochasticUniversalSampling selection, PopulationFitnessVector.Integer pf) {
+		int[] selected = new int[pf.size()];
+		selection.select(pf, selected);
+		int[] expectedMin = new int[pf.size()];
+		int[] expectedMax = new int[pf.size()];
+		int[] counts = new int[pf.size()];
+		for (int i = 0; i < pf.size(); i++) {
+			counts[selected[i]]++;
+		}
+		double[] runningSum = selection.computeWeightRunningSum(pf);
+		expectedMin[0] = (int)(pf.size() * runningSum[0] / runningSum[runningSum.length-1]);
+		expectedMax[0] = (int)Math.ceil(pf.size() * runningSum[0] / runningSum[runningSum.length-1]);
+		for (int i = 1; i < pf.size(); i++) {
+			expectedMin[i] = (int)(pf.size() * (runningSum[i]-runningSum[i-1]) / runningSum[runningSum.length-1]);
+			expectedMax[i] = (int)Math.ceil(pf.size() * (runningSum[i]-runningSum[i-1]) / runningSum[runningSum.length-1]);
+		}
+		for (int i = 0; i < pf.size(); i++) {
+			assertTrue("i:"+i+" count:"+counts[i]+" min:"+expectedMin[i], counts[i] >= expectedMin[i]);
+			assertTrue("i:"+i+" count:"+counts[i]+" max:"+expectedMax[i], counts[i] <= expectedMax[i]);
+		}
+	}
+	
+	private void validateExpectedCountsSUSWithRanks(StochasticUniversalSampling selection, PopulationFitnessVector.Double pf) {
+		int[] selected = new int[pf.size()];
+		selection.select(pf, selected);
+		int[] expectedMin = new int[pf.size()];
+		int[] expectedMax = new int[pf.size()];
+		int[] counts = new int[pf.size()];
+		for (int i = 0; i < pf.size(); i++) {
+			counts[selected[i]]++;
+		}
+		double[] runningSum = selection.computeWeightRunningSum(pf);
+		expectedMin[0] = (int)(pf.size() * runningSum[0] / runningSum[runningSum.length-1]);
+		expectedMax[0] = (int)Math.ceil(pf.size() * runningSum[0] / runningSum[runningSum.length-1]);
+		for (int i = 1; i < pf.size(); i++) {
+			expectedMin[i] = (int)(pf.size() * (runningSum[i]-runningSum[i-1]) / runningSum[runningSum.length-1]);
+			expectedMax[i] = (int)Math.ceil(pf.size() * (runningSum[i]-runningSum[i-1]) / runningSum[runningSum.length-1]);
+		}
+		for (int i = 0; i < pf.size(); i++) {
 			assertTrue("i:"+i+" count:"+counts[i]+" min:"+expectedMin[i], counts[i] >= expectedMin[i]);
 			assertTrue("i:"+i+" count:"+counts[i]+" max:"+expectedMax[i], counts[i] <= expectedMax[i]);
 		}

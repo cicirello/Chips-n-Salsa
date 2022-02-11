c4dd4492f5d1cf66f25f114a02baf2436106ac1b
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-02-11 11:08:55 -0500

    Exponential rank selection

--- src/test/java/org/cicirello/search/evo/SelectionOperatorTests.java
+++ src/test/java/org/cicirello/search/evo/SelectionOperatorTests.java
@@ -29,6 +29,52 @@ import static org.junit.Assert.*;
 public class SelectionOperatorTests {
 	
 	@Test
+	public void testExponentialRankSelection() {
+		double[] cValues = {Math.ulp(0.0), 0.25, 0.5, 0.75, 1.0-Math.ulp(1.0)};
+		for (double c : cValues) {
+			ExponentialRankSelection selection = new ExponentialRankSelection(c);
+			validateIndexes_Double(selection, c >= 0.5);
+			validateIndexes_Integer(selection, c >= 0.5);
+			ExponentialRankSelection selection2 = selection.split();
+			validateIndexes_Double(selection2, c >= 0.5);
+			validateIndexes_Integer(selection2, c >= 0.5);
+		}
+		double c = 0.5;
+		ExponentialRankSelection selection = new ExponentialRankSelection(c);
+		validateHigherFitnessSelectedMoreOften_Double(selection);
+		validateHigherFitnessSelectedMoreOften_Integer(selection);
+		
+		for (int n = 2; n <= 8; n *= 2) {
+			PopFitVectorDouble f1 = new PopFitVectorDouble(n);
+			double[] weights = selection.computeWeightRunningSum(f1);
+			for (int i = n-1; i > 0; i--) {
+				double delta = weights[i] - weights[i-1];
+				double expectedDelta = Math.pow(c,n-(i+1));
+				assertEquals(expectedDelta, delta, 1E-10);
+			}
+			assertEquals(Math.pow(c,n-1), weights[0], 1E-10);
+			
+			PopFitVectorInteger f2 = new PopFitVectorInteger(n);
+			weights = selection.computeWeightRunningSum(f2);
+			for (int i = n-1; i > 0; i--) {
+				double delta = weights[i] - weights[i-1];
+				double expectedDelta = Math.pow(c,n-(i+1));
+				assertEquals(expectedDelta, delta, 1E-10);
+			}
+			assertEquals(Math.pow(c,n-1), weights[0], 1E-10);
+		}
+		
+		IllegalArgumentException thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> new ExponentialRankSelection(0.0)
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> new ExponentialRankSelection(1.0)
+		);
+	}
+	
+	@Test
 	public void testLinearRankSelection() {
 		double[] cValues = {1.0, 1.25, 1.5, 1.75, 2.0};
 		for (double c : cValues) {

e548d2d37e8a3513cf73f74d93d05587a99640ca
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-02-22 15:15:42 -0500

    Update LargestCommonSubgraph.java

--- src/main/java/org/cicirello/search/problems/LargestCommonSubgraph.java
+++ src/main/java/org/cicirello/search/problems/LargestCommonSubgraph.java
@@ -21,17 +21,53 @@
 package org.cicirello.search.problems;
 
 import org.cicirello.permutations.Permutation;
-import org.cicirello.representations.BitVector;
+import org.cicirello.search.representations.BitVector;
 import java.util.SplittableRandom;
 import java.util.ArrayList;
 
-
-
+/**
+ * <p>This class is an implementation of the Largest Common Subgraph
+ * problem, an NP-Hard combinatorial optimization problem. In the problem,
+ * we are given two graphs G<sub>1</sub> and G<sub>2</sub>. The problem is to
+ * find the largest graph G<sub>c</sub>, measured in number of edges,
+ * that is isomorphic to a subgraph of G<sub>1</sub> and a subgraph of G<sub>2</sub>.</p>
+ *
+ * <p>The {@link #value value} method computes the actual optimization objective,
+ * number of edges in common, which we must maximize. The {@link #cost cost} method
+ * transforms this to a minimization problem by subtracting that count from 
+ * the number of edges in the smaller of G<sub>1</sub> and G<sub>2</sub> (in terms of
+ * edges).</p>
+ *
+ * <p>This implementation assumes representing solutions with a Permutation used to
+ * represent a mapping between the vertexes of the two graphs. Specifically, holding
+ * the vertexes of the graph with the fewer number of vertexes in a fixed order
+ * by their vertex id, a solution to the problem is represented with a Permutation of
+ * the vertex ids of the other graph. Position in the permutation corresponds to the
+ * mapping. For example, if p(i) = j, then vertex i in G<sub>1</sub> is mapped to
+ * vertex j in G<sub>2</sub>. This assumes that G<sub>1</sub> has at most the number
+ * of vertexes as G<sub>2</sub>.</p>
+ *
+ * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
+ * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
+ */
 public final class LargestCommonSubgraph implements IntegerCostOptimizationProblem<Permutation> {
 	
 	private final ArrayList<Edge> edgesG1;
 	private final BitVector[] adjacencyMatrixG2;
+	private int bound;
 	
+	/**
+	 * Constructs a random instance of the largest common subgraph problem.
+	 *
+	 * @param v The number of vertexes of each graph.
+	 * @param density The density of each graph, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 * @param isomorphic If true, the two graphs will be isomorphic, which provides an easy way
+	 * of generating instances with a known optimal solution.
+	 *
+	 * @throws IllegalArgumentException if v  is less than 1.
+	 * @throws IllegalArgumentException if density is less than 0.0 or greater than 1.0.
+	 */
 	public LargestCommonSubgraph(int v, double density, boolean isomorphic) {
 		this(v);
 		if (isomorphic) {
@@ -41,6 +77,19 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 		}
 	}
 	
+	/**
+	 * Constructs a random instance of the largest common subgraph problem.
+	 *
+	 * @param v1 The number of vertexes of graph 1.
+	 * @param v2 The number of vertexes of graph 2.
+	 * @param density1 The density of graph 1, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 * @param density2 The density of graph 2, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 *
+	 * @throws IllegalArgumentException if v1 and/or v2 is less than 1.
+	 * @throws IllegalArgumentException if either density1 or density2 is less than 0.0 or greater than 1.0.
+	 */
 	public LargestCommonSubgraph(int v1, int v2, double density1, double density2) {
 		this(v2 > v1 ? v2 : v1);
 		if (v1 < v2 || (v1 == v2 && density1 <= density2)) {
@@ -50,6 +99,19 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 		}
 	}
 	
+	/**
+	 * Constructs a random instance of the largest common subgraph problem.
+	 *
+	 * @param v The number of vertexes of each graph.
+	 * @param density The density of each graph, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 * @param isomorphic If true, the two graphs will be isomorphic, which provides an easy way
+	 * of generating instances with a known optimal solution.
+	 * @param seed The seed for the random number generator to enable replicating an instance.
+	 *
+	 * @throws IllegalArgumentException if v  is less than 1.
+	 * @throws IllegalArgumentException if density is less than 0.0 or greater than 1.0.
+	 */
 	public LargestCommonSubgraph(int v, double density, boolean isomorphic, long seed) {
 		this(v);
 		if (isomorphic) {
@@ -59,6 +121,20 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 		}
 	}
 	
+	/**
+	 * Constructs a random instance of the largest common subgraph problem.
+	 *
+	 * @param v1 The number of vertexes of graph 1.
+	 * @param v2 The number of vertexes of graph 2.
+	 * @param density1 The density of graph 1, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 * @param density2 The density of graph 2, which is the probability of an edge existing between 
+	 * a pair of vertexes. It must be in the interval [0.0, 1.0].
+	 * @param seed The seed for the random number generator to enable replicating an instance.
+	 *
+	 * @throws IllegalArgumentException if v1 and/or v2 is less than 1.
+	 * @throws IllegalArgumentException if either density1 or density2 is less than 0.0 or greater than 1.0.
+	 */
 	public LargestCommonSubgraph(int v1, int v2, double density1, double density2, long seed) {
 		this(v2 > v1 ? v2 : v1);
 		if (v1 < v2 || (v1 == v2 && density1 <= density2)) {
@@ -85,7 +161,7 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 	
 	@Override
 	public int cost(Permutation candidate) {
-		return edgesG1.size() - value(candidate)
+		return bound - value(candidate);
 	}
 	
 	@Override
@@ -127,6 +203,7 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 				}
 			}
 		}
+		bound = edgesG1.size();
 	}
 	
 	private void createRandomInstanceData(int v1, int v2, double density1, double density2, SplittableRandom gen) {
@@ -149,13 +226,18 @@ public final class LargestCommonSubgraph implements IntegerCostOptimizationProbl
 		for (int i = 0; i < v2; i++) {
 			adjacencyMatrixG2[i] = new BitVector(v2);
 		}
+		bound = 0;
 		for (int i = 0; i < v2; i++) {
 			for (int j = i+1; j < v2; j++) {
-				if (gen.nextDouble() < density1) {
+				if (gen.nextDouble() < density2) {
 					adjacencyMatrixG2[i].flip(j);
 					adjacencyMatrixG2[j].flip(i);
+					bound++;
 				}
 			}
+		}
+		if (edgesG1.size() < bound) {
+			bound = edgesG1.size();
 		}
 	}
 	

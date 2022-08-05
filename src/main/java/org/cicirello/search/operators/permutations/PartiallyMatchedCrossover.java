9531e1584259f8320f17ce8f8fc521e27df3f612
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-08-05 16:48:26 -0400

    documentation

--- src/main/java/org/cicirello/search/operators/permutations/PartiallyMatchedCrossover.java
+++ src/main/java/org/cicirello/search/operators/permutations/PartiallyMatchedCrossover.java
@@ -24,7 +24,40 @@ import org.cicirello.search.operators.CrossoverOperator;
 import org.cicirello.permutations.Permutation;
 import org.cicirello.math.rand.RandomIndexer;
 
-
+/**
+ *<p>Implementation of partially matched crossover (PMX). Partially matched crossover begins
+ * by selecting a random segment, much like a 2-point crossover for bit-strings. PMX then initializes
+ * the children as copies of the parents, and proceeds to make a sequence of swaps within child c1
+ * as to cause c1 at the end of those swaps to contain the cross region of p2 within its cross region,.
+ * And similarly for the other child.</p>
+ *
+ * <p>Consider as an example parent permutation p1 = [0, 1, 2, 3, 4, 5, 6, 7] and parent
+ * permutation p2 = [1, 2, 0, 5, 6, 7, 4, 3]. Now consider that the random cross region begins
+ * at index 2 and ends at index 4, inclusive. Child c1 is initialized as a copy of p1, 
+ * c1 = [0, 1, 2, 3, 4, 5, 6, 7], and we then swap the 2 with the 0 (the elements at index 2 in the parents)
+ * to get c1 = [2, 1, 0, 3, 4, 5, 6, 7]. Next, we swap the 3 with the 5 (the elements at index 3 in the parents)
+ * to get c1 = [2, 1, 0, 5, 4, 3, 6, 7]. Finally, we swap the 4 with the 6 (the elements at index 4 in the parents)
+ * to end up with c1 = [2, 1, 0, 5, 6, 3, 4, 7]. In a similar way, we initialize c2 as a copy of p2 and proceed with
+ * the designated swaps to end up with c2 = [1, 0, 2, 3, 4, 7, 6, 5].</p>
+ *
+ * <p>PMX was introduced in the following paper:<br>
+ * Goldberg, D.E. and Lingle, R. Alleles, Loci, and the Traveling Salesman Problem. <i>Proceedings of the 1st 
+ * International Conference on Genetic Algorithms</i>, 1985, pp. 154-159.</p>
+ *
+ * <p>Although, we actually relied on the seminal book on genetic algorithms by one of PMX's authors David E Goldberg:<br>
+ * Goldberg, D.E. <i>Genetic Algorithms in Search, Optimization and Machine Learning</i>, Addison Wesley, 1989.</p>
+ *
+ * <p>Note that this implementation in Chips-n-Salsa is asymptotically faster than Goldberg and Lingle's algorithmic 
+ * description of PMX. In the original PMX description, the indexes of the elements to swap were found with a linear search,
+ * and since on average there is a linear number of these, PMX as originally described required O(n<sup>2</sup>) time.
+ * However, the implementation here in Chips-n-Salsa computes the inverse of each permutation in linear time, which is then used
+ * as a lookup table for the indexes of the elements to swap. Each swap is constant time, and also involves a constant time
+ * update to the lookup table of indexes. Thus, this implementation has an average case and worse case runtime O(n), where
+ * n is permutation length.</p>
+ *
+ * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
+ * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
+ */
 public final class PartiallyMatchedCrossover implements CrossoverOperator<Permutation> {
 	
 	/**

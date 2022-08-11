95196e711f03dfa0f351d439d46cf841012adc65
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-08-11 11:54:01 -0400

    refactored: extracted internalCross from cross

--- src/main/java/org/cicirello/search/operators/permutations/PrecedencePreservativeCrossover.java
+++ src/main/java/org/cicirello/search/operators/permutations/PrecedencePreservativeCrossover.java
@@ -73,54 +73,7 @@ public final class PrecedencePreservativeCrossover implements CrossoverOperator<
 	@Override
 	public void cross(Permutation c1, Permutation c2) {
 		c1.apply( 
-			(raw1, raw2) -> {
-				int[] old1 = raw1.clone();
-				int[] old2 = raw2.clone();
-				boolean[] used1 = new boolean[raw1.length];
-				boolean[] used2 = new boolean[raw1.length];
-				
-				int i = RandomIndexer.nextInt(raw1.length);
-				int j = RandomIndexer.nextInt(raw1.length);
-				int otherCount = Math.abs(i-j) + 1;
-				i = Math.min(i, j);
-				
-				int k = 0;
-				while (k < i) {
-					used1[raw1[k]] = true;
-					used2[raw2[k]] = true;
-					k++;
-				}
-				int x = i;
-				i = j = 0;
-				while (otherCount > 0) {
-					while (used1[old2[i]]) {
-						i++;
-					}
-					while (used2[old1[j]]) {
-						j++;
-					}
-					used1[raw1[k] = old2[i]] = true;
-					used2[raw2[k] = old1[j]] = true;
-					k++;
-					i++;
-					j++;
-					otherCount--;
-				}
-				i = j = x;
-				while (k < raw1.length) {
-					while (used1[old1[i]]) {
-						i++;
-					}
-					while (used2[old2[j]]) {
-						j++;
-					}
-					used1[raw1[k] = old1[i]] = true;
-					used2[raw2[k] = old2[j]] = true;
-					i++;
-					j++;
-					k++;
-				}
-			},
+			(raw1, raw2) -> internalCross(raw1, raw2, RandomIndexer.nextInt(raw1.length), RandomIndexer.nextInt(raw1.length)),
 			c2
 		);
 	}
@@ -129,5 +82,55 @@ public final class PrecedencePreservativeCrossover implements CrossoverOperator<
 	public PrecedencePreservativeCrossover split() {
 		// doesn't maintain any state, so safe to return this
 		return this;
+	}
+	
+	/*
+	 * package private to facilitate testing
+	 */
+	final void internalCross(int[] raw1, int[] raw2, int i, int j) {
+		int[] old1 = raw1.clone();
+		int[] old2 = raw2.clone();
+		boolean[] used1 = new boolean[raw1.length];
+		boolean[] used2 = new boolean[raw1.length];
+		
+		int otherCount = Math.abs(i-j) + 1;
+		i = Math.min(i, j);
+		
+		int k = 0;
+		while (k < i) {
+			used1[raw1[k]] = true;
+			used2[raw2[k]] = true;
+			k++;
+		}
+		int x = i;
+		i = j = 0;
+		while (otherCount > 0) {
+			while (used1[old2[i]]) {
+				i++;
+			}
+			while (used2[old1[j]]) {
+				j++;
+			}
+			used1[raw1[k] = old2[i]] = true;
+			used2[raw2[k] = old1[j]] = true;
+			k++;
+			i++;
+			j++;
+			otherCount--;
+		}
+		i = j = x;
+		while (k < raw1.length) {
+			while (used1[old1[i]]) {
+				i++;
+			}
+			while (used2[old2[j]]) {
+				j++;
+			}
+			used1[raw1[k] = old1[i]] = true;
+			used2[raw2[k] = old2[j]] = true;
+			i++;
+			j++;
+			k++;
+		}
 	}
 }

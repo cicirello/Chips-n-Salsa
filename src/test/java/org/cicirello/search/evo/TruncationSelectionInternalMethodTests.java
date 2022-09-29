2d03b6fd89f12de9b2211a9c2748b490a940267c
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-09-29 10:24:05 -0400

    refactor tests

--- src/test/java/org/cicirello/search/evo/TruncationSelectionInternalMethodTests.java
+++ src/test/java/org/cicirello/search/evo/TruncationSelectionInternalMethodTests.java
@@ -35,151 +35,99 @@ public class TruncationSelectionInternalMethodTests extends SharedTestSelectionO
 			for (int n = 1; n <= 8; n++) {
 				int truncateCount = Math.max(n - k, 0);
 				PopFitVectorIntegerSimple pop1 = new PopFitVectorIntegerSimple(n);
-				int[] partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				boolean[] found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= truncateCount);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) > truncateCount);
-				}
-				
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					truncateCount,
+					truncateCount + 1,
+					false,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= truncateCount);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) > truncateCount);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					truncateCount,
+					truncateCount + 1,
+					false,
+					false
+				);
 				
 				// All duplicates low fitness
-				int[] fits = new int[n];
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount) {
-						fits[i] = 1;
-					} else {
-						fits[i] = 2 + i;
-					}
-				}
-				pop1 = new PopFitVectorIntegerSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[2+n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertEquals(1, pop1.getFitness(partitioned[i]));
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 2);
-				}
-				
+				pop1 = new PopFitVectorIntegerSimple(lowDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					1,
+					2,
+					true,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[2+n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertEquals(1, pop1.getFitness(partitioned[i]));
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 2);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					1,
+					2,
+					true,
+					false
+				);
 				
 				// all duplicates high fitness
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount) {
-						fits[i] = 1 + i;
-					} else {
-						fits[i] = 100;
-					}
-				}
-				pop1 = new PopFitVectorIntegerSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) < 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertEquals(100, pop1.getFitness(partitioned[i]));
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				
+				pop1 = new PopFitVectorIntegerSimple(highDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					99,
+					100,
+					false,
+					true
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) < 100, "k: " + k + " partitioned: " + toString(partitioned) + "  fits: " + toString(fits));
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertEquals(100, pop1.getFitness(partitioned[i]));
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					99,
+					100,
+					false,
+					true
+				);
 				
 				// duplicates surrounding boundary
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount - 1) {
-						fits[i] = 1 + i;
-					} else if (i < truncateCount + 2) {
-						fits[i] = 100;
-					} else {
-						fits[i] = 200 + i;
-					}
-				}
-				pop1 = new PopFitVectorIntegerSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 100);
-				}
-				
+				pop1 = new PopFitVectorIntegerSimple(boundaryDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n, 
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount), 
+					pop1, 
+					100, 
+					100,
+					false,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 100);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n, 
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount), 
+					pop1, 
+					100, 
+					100,
+					false,
+					false
+				);
 			}
 		}
 	}
@@ -191,151 +139,99 @@ public class TruncationSelectionInternalMethodTests extends SharedTestSelectionO
 			for (int n = 1; n <= 8; n++) {
 				int truncateCount = Math.max(n - k, 0);
 				PopFitVectorDoubleSimple pop1 = new PopFitVectorDoubleSimple(n);
-				int[] partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				boolean[] found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= truncateCount);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) > truncateCount);
-				}
-				
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					truncateCount,
+					truncateCount + 1,
+					false,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= truncateCount);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) > truncateCount);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					truncateCount,
+					truncateCount + 1,
+					false,
+					false
+				);
 				
 				// All duplicates low fitness
-				int[] fits = new int[n];
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount) {
-						fits[i] = 1;
-					} else {
-						fits[i] = 2 + i;
-					}
-				}
-				pop1 = new PopFitVectorDoubleSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[2+n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertEquals(1.0, pop1.getFitness(partitioned[i]), 0.0);
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 2);
-				}
-				
+				pop1 = new PopFitVectorDoubleSimple(lowDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					1,
+					2,
+					true,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[2+n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertEquals(1.0, pop1.getFitness(partitioned[i]), 0.0);
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 2);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					1,
+					2,
+					true,
+					false
+				);
 				
 				// all duplicates high fitness
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount) {
-						fits[i] = 1 + i;
-					} else {
-						fits[i] = 100;
-					}
-				}
-				pop1 = new PopFitVectorDoubleSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) < 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertEquals(100.0, pop1.getFitness(partitioned[i]), 0.0);
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
-				
+				pop1 = new PopFitVectorDoubleSimple(highDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					99,
+					100,
+					false,
+					true
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) < 100, "k: " + k + " partitioned: " + toString(partitioned) + "  fits: " + toString(fits));
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertEquals(100.0, pop1.getFitness(partitioned[i]), 0.0);
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n,
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount),
+					pop1,
+					99,
+					100,
+					false,
+					true
+				);
 				
 				// duplicates surrounding boundary
-				for (int i = 0; i < n; i++) {
-					if (i < truncateCount - 1) {
-						fits[i] = 1 + i;
-					} else if (i < truncateCount + 2) {
-						fits[i] = 100;
-					} else {
-						fits[i] = 200 + i;
-					}
-				}
-				pop1 = new PopFitVectorDoubleSimple(fits);
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 100);
-				}
-				
+				pop1 = new PopFitVectorDoubleSimple(boundaryDuplicatesTestData(truncateCount, n));
+				verifyBestToRight(
+					truncateCount, 
+					n, 
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount), 
+					pop1, 
+					100, 
+					100,
+					false,
+					false
+				);
 				pop1.reverse();
-				partitioned = selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount);
-				assertEquals(n, partitioned.length);
-				found = new boolean[n];
-				for (int i = 0; i < truncateCount; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) <= 100);
-				}
-				for (int i = truncateCount; i < n; i++) {
-					assertFalse(found[partitioned[i]]);
-					found[partitioned[i]] = true;
-					assertTrue(pop1.getFitness(partitioned[i]) >= 100);
-				}
+				verifyBestToRight(
+					truncateCount, 
+					n, 
+					selection.bestFitToRight(pop1, selection.initSelectFrom(n), 0, n-1, truncateCount), 
+					pop1, 
+					100, 
+					100,
+					false,
+					false
+				);
 			}
 		}
 	}
@@ -346,5 +242,89 @@ public class TruncationSelectionInternalMethodTests extends SharedTestSelectionO
 			s += " " + array[i];
 		}
 		return s;
+	}
+	
+	private void verifyBestToRight(int truncateCount, int n, int[] partitioned, PopFitVectorDoubleSimple pop1, int firstLimit, int secondLimit, boolean firstExact, boolean secondExact) {
+		assertEquals(n, partitioned.length);
+		boolean[] found = new boolean[n];
+		for (int i = 0; i < truncateCount; i++) {
+			assertFalse(found[partitioned[i]]);
+			found[partitioned[i]] = true;
+			if (firstExact) {
+				assertEquals(firstLimit, pop1.getFitness(partitioned[i]));
+			} else {
+				assertTrue(pop1.getFitness(partitioned[i]) <= firstLimit);
+			}
+		}
+		for (int i = truncateCount; i < n; i++) {
+			assertFalse(found[partitioned[i]]);
+			found[partitioned[i]] = true;
+			if (secondExact) {
+				assertEquals(secondLimit, pop1.getFitness(partitioned[i]));
+			} else {
+				assertTrue(pop1.getFitness(partitioned[i]) >= secondLimit);
+			}
+		}
+	}
+	
+	private void verifyBestToRight(int truncateCount, int n, int[] partitioned, PopFitVectorIntegerSimple pop1, int firstLimit, int secondLimit, boolean firstExact, boolean secondExact) {
+		assertEquals(n, partitioned.length);
+		boolean[] found = new boolean[n];
+		for (int i = 0; i < truncateCount; i++) {
+			assertFalse(found[partitioned[i]]);
+			found[partitioned[i]] = true;
+			if (firstExact) {
+				assertEquals(firstLimit, pop1.getFitness(partitioned[i]));
+			} else {
+				assertTrue(pop1.getFitness(partitioned[i]) <= firstLimit);
+			}
+		}
+		for (int i = truncateCount; i < n; i++) {
+			assertFalse(found[partitioned[i]]);
+			found[partitioned[i]] = true;
+			if (secondExact) {
+				assertEquals(secondLimit, pop1.getFitness(partitioned[i]));
+			} else {
+				assertTrue(pop1.getFitness(partitioned[i]) >= secondLimit);
+			}
+		}
+	}
+	
+	private int[] lowDuplicatesTestData(int truncateCount, int n) {
+		int[] fits = new int[n];
+		for (int i = 0; i < n; i++) {
+			if (i < truncateCount) {
+				fits[i] = 1;
+			} else {
+				fits[i] = 2 + i;
+			}
+		}
+		return fits;
+	}
+	
+	private int[] highDuplicatesTestData(int truncateCount, int n) {
+		int[] fits = new int[n];
+		for (int i = 0; i < n; i++) {
+			if (i < truncateCount) {
+				fits[i] = 1 + i;
+			} else {
+				fits[i] = 100;
+			}
+		}
+		return fits;
+	}
+	
+	private int[] boundaryDuplicatesTestData(int truncateCount, int n) {
+		int[] fits = new int[n];
+		for (int i = 0; i < n; i++) {
+			if (i < truncateCount - 1) {
+				fits[i] = 1 + i;
+			} else if (i < truncateCount + 2) {
+				fits[i] = 100;
+			} else {
+				fits[i] = 200 + i;
+			}
+		}
+		return fits;
 	}
 }

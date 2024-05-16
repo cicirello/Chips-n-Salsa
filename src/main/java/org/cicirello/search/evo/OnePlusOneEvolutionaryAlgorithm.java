b0c0821ab778bd7607852a537acdc5ad14786050
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2024-05-16 15:26:58 -0400

    prevent finalizer vulnerability

--- src/main/java/org/cicirello/search/evo/OnePlusOneEvolutionaryAlgorithm.java
+++ src/main/java/org/cicirello/search/evo/OnePlusOneEvolutionaryAlgorithm.java
@@ -1,6 +1,6 @@
 /*
  * Chips-n-Salsa: A library of parallel self-adaptive local search algorithms.
- * Copyright (C) 2002-2023 Vincent A. Cicirello
+ * Copyright (C) 2002-2024 Vincent A. Cicirello
  *
  * This file is part of Chips-n-Salsa (https://chips-n-salsa.cicirello.org/).
  *
@@ -155,6 +155,14 @@ public class OnePlusOneEvolutionaryAlgorithm<T extends Copyable<T>>
     mutation = other.mutation.split();
 
     sr = pOptInt != null ? new IntCostSingleRun() : new DoubleCostSingleRun();
+  }
+
+  @SuppressWarnings("deprecation")
+  @Override
+  protected final void finalize() {
+    // Prevents potential finalizer vulnerability from exceptions thrown from constructors.
+    // See:
+    // https://wiki.sei.cmu.edu/confluence/display/java/OBJ11-J.+Be+wary+of+letting+constructors+throw+exceptions
   }
 
   /**

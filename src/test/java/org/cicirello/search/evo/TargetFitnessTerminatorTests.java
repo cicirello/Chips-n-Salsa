2de02aee8f134e8074141b7a52c8e3a69a25b543
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2026-09-09 16:11:27 -0400

    add TargetFitnessTerminator

---
+++ src/test/java/org/cicirello/search/evo/TargetFitnessTerminatorTests.java
@@ -0,0 +1,57 @@
+/*
+ * Chips-n-Salsa: A library of parallel self-adaptive local search algorithms.
+ * Copyright (C) 2002-2026 Vincent A. Cicirello
+ *
+ * This file is part of Chips-n-Salsa (https://chips-n-salsa.cicirello.org/).
+ *
+ * Chips-n-Salsa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chips-n-Salsa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.cicirello.search.evo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/** JUnit test cases for TargetFitnessTerminator interface. */
+public class TargetFitnessTerminatorTests {
+
+  @Test
+  public void test() {
+    TargetFitnessTerminator<String> terminator = new TargetFitnessTerminator<String>(5);
+
+    int numCompletedGenerations = 1;
+    long fitnessEvaluations = 10;
+    String bestSolution = "hello";
+    double bestFitness = 5;
+
+    TerminationStrategy.ExecutionState<String> ex =
+        new TerminationStrategy.ExecutionState<String>(
+            numCompletedGenerations, fitnessEvaluations, bestSolution, bestFitness, null);
+
+    assertTrue(terminator.terminate(ex));
+
+    bestFitness = 4;
+    ex =
+        new TerminationStrategy.ExecutionState<String>(
+            numCompletedGenerations, fitnessEvaluations, bestSolution, bestFitness, null);
+    assertFalse(terminator.terminate(ex));
+
+    bestFitness = 6;
+    ex =
+        new TerminationStrategy.ExecutionState<String>(
+            numCompletedGenerations, fitnessEvaluations, bestSolution, bestFitness, null);
+    assertTrue(terminator.terminate(ex));
+  }
+}
